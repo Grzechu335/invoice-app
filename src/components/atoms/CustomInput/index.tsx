546532ddc946ebd85d765f3c58087e2bc1054d34
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-06-05 01:32:17 +0200

    UI fix

--- src/components/atoms/CustomInput/index.tsx
+++ src/components/atoms/CustomInput/index.tsx
@@ -2,16 +2,35 @@ import React, { HTMLAttributes } from 'react'
 
 interface CustomInputProps extends HTMLAttributes<HTMLInputElement> {
     label?: string
+    labelOnSmallDevices?: boolean
+    disabled?: boolean
 }
 
 export const CustomInput = React.forwardRef<HTMLInputElement, CustomInputProps>(
     (props, ref) => {
-        const { label, className, ...rest } = props
+        const {
+            label,
+            className,
+            disabled = false,
+            labelOnSmallDevices = false,
+            ...rest
+        } = props
         return (
             <label className={`${className}`}>
-                <p className="mb-2 text-color-7 body-variant">{label}</p>
+                <p
+                    className={`mb-2 text-color-7 body-variant ${
+                        labelOnSmallDevices ? 'md:hidden' : ''
+                    }`}
+                >
+                    {label}
+                </p>
                 <input
-                    className={`rounded-[4px] bg-white heading-sm-variant dark:bg-color-3 border-[1px] w-full outline-none border-[#DFE3FA] dark:border-[#252945] px-5 py-4 `}
+                    className={`rounded-[4px] bg-white heading-sm-variant  border-[1px] w-full outline-none border-[#DFE3FA] dark:border-[#252945] px-5 py-4 ${
+                        disabled
+                            ? 'border-none text-color-6 px-0 dark:bg-color-12'
+                            : 'dark:bg-color-3 px-5'
+                    } `}
+                    disabled={disabled}
                     {...rest}
                     ref={ref}
                 />
--- src/components/organisms/Form/index.tsx
+++ src/components/organisms/Form/index.tsx
@@ -7,6 +7,8 @@ import CustomDatePicker from '../CustomDatePicker'
 import { Invoice } from '../../../../types/invoice'
 import CustomSelect from '../CustomSelect'
 import CustomButton from '@/components/atoms/CustomButton'
+import DeleteIcon from 'public/assets/icon-delete.svg'
+import Image from 'next/image'
 
 type FormProps = {
     invoice?: Invoice
@@ -102,22 +104,44 @@ const Form: React.FC<FormProps> = ({ invoice }) => {
                 <h4 className="font-bold text-[18px] leading-[32px] tracking-[-0.375px] text-[#777F98] mb-4">
                     Item List
                 </h4>
-                <div className="grid grid-cols-12 gap-4 body-variant text-color-7">
+                <div className="hidden grid-cols-12 gap-4 mb-4 body-variant text-color-7 md:grid">
                     <p className="col-span-4">Item name</p>
                     <p className="col-span-2">Qty.</p>
-                    <p className="col-span-3">Price</p>
+                    <p className="col-span-2">Price</p>
                     <p className="col-span-3">Total</p>
                 </div>
                 <div className="grid grid-cols-12 gap-4">
-                    <CustomInput className="col-span-4" />
-                    <CustomInput className="col-span-2" />
-                    <CustomInput className="col-span-3" />
-                    <CustomInput className="col-span-3" />
+                    <CustomInput
+                        labelOnSmallDevices
+                        label="Item name"
+                        className="col-span-full md:col-span-4"
+                    />
+                    <CustomInput
+                        labelOnSmallDevices
+                        label="Qty."
+                        className="col-span-3 md:col-span-2"
+                    />
+                    <CustomInput
+                        labelOnSmallDevices
+                        label="Price"
+                        className="col-span-4 md:col-span-2"
+                    />
+                    <CustomInput
+                        labelOnSmallDevices
+                        disabled
+                        label="Total"
+                        className="col-span-4 md:col-span-3"
+                    />
+                    <Image
+                        src={DeleteIcon}
+                        alt="delete item icon"
+                        className="relative w-[18px] mx-auto my-auto md:static top-3"
+                    />
                 </div>
                 <div>
                     <CustomButton
                         variant={6}
-                        className="flex items-center justify-center w-full mt-4"
+                        className="flex items-center justify-center w-full mt-12 md:mt-4"
                     >
                         + Add New Item
                     </CustomButton>
