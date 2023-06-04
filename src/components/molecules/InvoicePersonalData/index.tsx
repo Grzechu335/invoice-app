d045d19fddbf2c25de6c235fe2c1708b9a185872
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-06-04 17:04:43 +0200

    Styled invoice details section

--- src/components/molecules/InvoicePersonalData/index.tsx
+++ src/components/molecules/InvoicePersonalData/index.tsx
@@ -9,40 +9,62 @@ const InvoicePersonalData: React.FC<Invoice> = ({
     createdAt,
     paymentDue,
     clientAddress,
+    clientName,
 }) => {
     return (
-        <div className="grid md:grid-cols-4 md:grid-rows-3 grid-cols-2 grid-rows-[1fr_2fr_1fr_1fr_1fr] grid-areas-invoiceDetailsSmall md:grid-areas-invoiceDetailsWide">
-            <div className="grid-in-id">
-                <p>
-                    <span>#</span>
+        <div className="grid md:grid-cols-4 md:grid-rows-3 gap-2 md:gap-4 grid-cols-2 grid-rows-[1fr_2fr_1fr_1fr_1fr] grid-areas-invoiceDetailsSmall md:grid-areas-invoiceDetailsWide">
+            <div className="flex flex-col space-y-2 grid-in-id">
+                <p className="text-color-8 dark:text-[#fff] heading-sm">
+                    <span className="text-color-6">#</span>
                     {id}
                 </p>
-                <p>{description}</p>
+                <p className="text-color-7 dark:text-color-5 body-variant">
+                    {description}
+                </p>
             </div>
-            <div className="flex flex-col grid-in-sender">
+            <div className="flex flex-col grid-in-sender text-color-7 text-start md:text-end dark:text-color-5 body-variant">
                 <p>{senderAddress.street}</p>
                 <p>{senderAddress.city}</p>
                 <p>{senderAddress.postCode}</p>
                 <p>{senderAddress.country}</p>
             </div>
-            <div className="grid-in-invoiceDate">
-                <p>Invoice Date</p>
-                <p>{createdAt}</p>
+            <div className="flex flex-col space-y-3 grid-in-invoiceDate">
+                <p className="text-color-7 dark:text-color-5 body-variant">
+                    Invoice Date
+                </p>
+                <p className="text-color-8 dark:text-[#fff] heading-sm">
+                    {createdAt}
+                </p>
             </div>
-            <div className="grid-in-paymentDue">
-                <p>Payment Due</p>
-                <p>{paymentDue}</p>
+            <div className="flex flex-col space-y-3 grid-in-paymentDue">
+                <p className="text-color-7 dark:text-color-5 body-variant">
+                    Payment Due
+                </p>
+                <p className="text-color-8 dark:text-[#fff] heading-sm">
+                    {paymentDue}
+                </p>
             </div>
-            <div className="grid-in-client">
-                <p>Bill to</p>
-                <p>{clientAddress.street}</p>
-                <p>{clientAddress.city}</p>
-                <p>{clientAddress.postCode}</p>
-                <p>{clientAddress.country}</p>
+            <div className="flex flex-col grid-in-client text-end md:text-start">
+                <p className="text-color-7 dark:text-color-5 body-variant">
+                    Bill to
+                </p>
+                <p className="text-color-8 dark:text-[#fff] heading-sm mt-3">
+                    {clientName}
+                </p>
+                <div className="mt-2">
+                    <p>{clientAddress.street}</p>
+                    <p>{clientAddress.city}</p>
+                    <p>{clientAddress.postCode}</p>
+                    <p>{clientAddress.country}</p>
+                </div>
             </div>
             <div className="grid-in-mail">
-                <p>Sent to</p>
-                <p>{clientEmail}</p>
+                <p className="text-color-7 dark:text-color-5 body-variant">
+                    Sent to
+                </p>
+                <p className="text-color-8 dark:text-[#fff] heading-sm mt-3">
+                    {clientEmail}
+                </p>
             </div>
         </div>
     )
--- src/components/organisms/InvoiceItems/index.tsx
+++ src/components/organisms/InvoiceItems/index.tsx
@@ -6,32 +6,34 @@ type Props = {}
 
 const InvoiceItems: React.FC<Invoice> = ({ items, total }) => {
     return (
-        <div className="bg-[#F9FAFE] dark:bg-color-4 rounded-[8px]">
+        <div className="bg-[#F9FAFE] mt-12 dark:bg-color-4 rounded-[8px]">
             <div className="p-6">
-                <div className="hidden grid-cols-5 md:grid">
+                <div className="hidden grid-cols-5 md:grid md:mb-8 !text-color-7 body-variant dark:!text-color-5">
                     <p className="col-span-2">Item name</p>
                     <p className="text-center">QTY.</p>
                     <p className="text-end">Price</p>
                     <p className="text-end">Total</p>
                 </div>
-                <div>
+                <div className="flex flex-col space-y-6 md:space-y-8">
                     {items.map(({ name, price, quantity, total }, idx) => (
                         <React.Fragment key={idx}>
-                            <div className="hidden grid-cols-5 md:grid">
+                            <div className="hidden grid-cols-5 md:grid heading-sm-variant">
                                 <p className="md:col-span-2">{name}</p>
-                                <p className="md:text-center">{quantity}</p>
-                                <p className="md:text-end">
+                                <p className="md:text-center text-color-7 dark:text-color-5">
+                                    {quantity}
+                                </p>
+                                <p className="md:text-end text-color-7 dark:text-color-5">
                                     {currencyFormatter(price)}
                                 </p>
                                 <p className="md:text-end">
                                     {currencyFormatter(total)}
                                 </p>
                             </div>
-                            <div className="flex items-center justify-between md:hidden">
-                                <div className="flex flex-col">
+                            <div className="flex items-center justify-between md:hidden heading-sm-variant">
+                                <div className="flex flex-col space-y-2">
                                     <p>{name}</p>
-                                    <p>
-                                        {quantity} x {price}
+                                    <p className="text-color-7">
+                                        {quantity} x {currencyFormatter(price)}
                                     </p>
                                 </div>
                                 <div>
@@ -43,10 +45,10 @@ const InvoiceItems: React.FC<Invoice> = ({ items, total }) => {
                 </div>
             </div>
             <div className="bg-[#373B53] dark:bg-black rounded-b-[8px] p-6 flex items-center w-full">
-                <div className="flex items-center justify-between w-full">
+                <div className="flex items-center justify-between w-full text-[#fff] body-variant">
                     <p className="hidden md:inline">Amount Due</p>
                     <p className="md:hidden">Grand Total</p>
-                    <p>{currencyFormatter(total)}</p>
+                    <p className="heading-md">{currencyFormatter(total)}</p>
                 </div>
             </div>
         </div>
