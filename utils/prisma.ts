67a2efae1f2e4057e748e2072fab8c845df2c691
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-06-08 20:25:50 +0200

    Fixed typescript error /new version of ts installed/

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,13 +1,15 @@
+// // lib/prisma.ts
+
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient | undefined
+let prisma: PrismaClient
+if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient()
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient()
+    }
+    prisma = global.prisma
 }
 
-export const prisma =
-    globalForPrisma.prisma ??
-    new PrismaClient({
-        log: ['query'],
-    })
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export { prisma }
