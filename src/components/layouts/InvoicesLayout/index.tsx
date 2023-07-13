42f15b2c8f774263cbab20cc69e86bfbe79be7db
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-07-13 22:49:14 +0200

    Added animation to invoice layout

--- src/components/layouts/InvoicesLayout/index.tsx
+++ src/components/layouts/InvoicesLayout/index.tsx
@@ -2,6 +2,7 @@ import InvoiceItem from '@/components/organisms/InvoiceItem'
 import EmptyInvoiceTemplate from '@/components/templates/EmptyInvoiceTemplate'
 import React from 'react'
 import { dashboardInvoice } from '../../../../types/invoice'
+import { motion as m } from 'framer-motion'
 
 type InvoicesLayoutProps = {
     invoices: dashboardInvoice[]
@@ -10,14 +11,20 @@ type InvoicesLayoutProps = {
 const InvoicesLayout: React.FC<InvoicesLayoutProps> = ({ invoices }) => {
     if (invoices.length === 0) return <EmptyInvoiceTemplate />
     return (
-        <div className="xl:pt-[64px] md:pt-[55px] pt-[32px] flex flex-col space-y-[16px]">
+        <m.div
+            layout
+            transition={{
+                duration: 0.1,
+            }}
+            className="xl:pt-[64px] md:pt-[55px] pt-[32px] flex flex-col space-y-[16px] relative -z-10"
+        >
             {invoices.map((invoice) => (
                 <InvoiceItem
                     key={invoice.id}
                     {...invoice}
                 />
             ))}
-        </div>
+        </m.div>
     )
 }
 
