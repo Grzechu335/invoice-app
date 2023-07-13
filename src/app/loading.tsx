c013658c37f951d452db6fff0403c7622c2806f0
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-07-13 20:43:04 +0200

    Added loading spinner to home page

---
+++ src/app/loading.tsx
@@ -0,0 +1,25 @@
+'use client'
+import React from 'react'
+import { Oval } from 'react-loader-spinner'
+type Props = {}
+
+const loading = (props: Props) => {
+    return (
+        <div className="fixed inset-0 grid place-content-center">
+            <Oval
+                height={80}
+                width={80}
+                color="#9277ff"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="oval-loading"
+                secondaryColor="#7c5dfa"
+                strokeWidth={2}
+                strokeWidthSecondary={2}
+            />
+        </div>
+    )
+}
+
+export default loading
