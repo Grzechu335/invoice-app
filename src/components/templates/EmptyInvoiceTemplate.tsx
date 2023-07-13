33b48e87c63b9d762e90d5edd35cfbec2ebd3e1a
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-07-13 23:17:41 +0200

    UI fixes

--- src/components/templates/EmptyInvoiceTemplate.tsx
+++ src/components/templates/EmptyInvoiceTemplate.tsx
@@ -7,7 +7,7 @@ type Props = {}
 const EmptyInvoiceTemplate = () => {
     return (
         <div className="fixed inset-0 grid mx-auto place-content-center">
-            <div className="w-[200px] space-y-[66px] relative top-[72px] md:top-[80px] xl::top-0">
+            <div className="w-[200px] space-y-[66px] relative top-[72px] md:top-[80px] xl:top-0">
                 <Image
                     src={emptyImage}
                     alt="no invoices image"
