db1af899766b049977adc31d36c9cd714ab05df4
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-07-14 23:13:56 +0200

    UI fixes

--- src/components/atoms/UserAvatar/index.tsx
+++ src/components/atoms/UserAvatar/index.tsx
@@ -18,8 +18,9 @@ const UserAvatar: React.FC = () => {
     ) : (
         <Skeleton className="w-[40px] h-[40px] rounded-full" />
     )
+    // border-l-[1px] border-l-[#494E6E] xl:border-l-0 xl:border-t-[1px] xl:border-t-[#494E6E]
     return (
-        <div className="border-l-[1px] border-l-[#494E6E] xl:border-l-0 xl:border-t-[1px] xl:border-t-[#494E6E] grid place-content-center px-[24px] xl:py-[24px]">
+        <div className="grid place-content-center px-[24px] xl:py-[24px] relative after:absolute after:w-[1px] after:inset-y-3 after:bg-[#494E6E] xl:after:inset-x-3 xl:after:top-0 xl:after:h-[1px] xl:after:w-auto">
             {avatar}
         </div>
     )
